280cbe93b4298206cc6bf2a3fcdf6f1a063aac33
smdirfan542 <139772914+smdirfan542@users.noreply.github.com>
2026-09-06 11:29:53 +0530

    Update auth.api.js

--- Frontend/src/Features/Auth/services/auth.api.js
+++ Frontend/src/Features/Auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:3000/auth',
+    baseURL:'https://social-posts-ltay.onrender.com/auth',
     withCredentials:true
 })
 
